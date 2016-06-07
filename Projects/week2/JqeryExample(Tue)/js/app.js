ac45bcfe7e7f51e579159d243915cf4ef14d79a2
CMrcm <jdcopela@umail.iu.edu>
2016-06-07 10:11:27 -0400

    with mutant-id as a data attribute

--- Projects/week2/JqeryExample(Tue)/js/app.js
+++ Projects/week2/JqeryExample(Tue)/js/app.js
@@ -1,12 +1,12 @@
 var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
-function processMutants(ajaxResults){
-	$.each(ajaxResults, function(i,mutant){
+function processMutants(mutants){
+	$.each(mutants, function(i,mutant){
 		addMutant(mutant);
 	});
 }
 
 function addMutant(mutant){
-	$('body').append('<li>' + mutant.mutant_name + '</li>');
+	$('#mutantList').append('<li data-id="'+ mutant. id +'">' + mutant.mutant_name + '</li>');
 }
 
 $.get({
