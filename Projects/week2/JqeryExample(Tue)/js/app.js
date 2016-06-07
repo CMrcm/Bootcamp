0db94031245263f48dd6395dcf5ee2da5aba6b5c
CMrcm <jdcopela@umail.iu.edu>
2016-06-07 10:44:14 -0400

    delete link works but not displaying changes

--- Projects/week2/JqeryExample(Tue)/js/app.js
+++ Projects/week2/JqeryExample(Tue)/js/app.js
@@ -1,4 +1,11 @@
 var mutantsUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants';
+
+$(document).on('click','a.delete',function(ev){ //even listener for any delete link via 'a'
+	var li = $(ev.currentTarget).closest('li');
+	var id = li.data('id');
+	deleteMutant(id);
+}); 
+
 function processMutants(mutants){
 	$.each(mutants, function(i,mutant){
 		addMutant(mutant);
